032cc53bbba85d79c43e6628c2a8cdaaca2d3ecc
Sean Wolfe <sean@iheartsquares.com>
2009-08-31 16:33:14 -0500

    Updated erlang to version R13B01. Also added ErlangManuals and ErlangHtmlDocs and correct version method.

--- Library/Formula/erlang.rb
+++ Library/Formula/erlang.rb
@@ -1,9 +1,23 @@
 require 'brewkit'
 
+class ErlangManuals <Formula
+  @url='http://erlang.org/download/otp_doc_man_R13B01.tar.gz'
+  @md5='fa8f96159bd9a88aa2fb9e4d79d7affe'
+end
+
+class ErlangHtmlDocs <Formula
+  @url='http://erlang.org/download/otp_doc_html_R13B01.tar.gz'
+  @md5='42cb55bbfa5dc071fd56034615072f7a'
+end
+
 class Erlang <Formula
   @homepage='http://www.erlang.org'
-  @url='http://erlang.org/download/otp_src_R13B.tar.gz'
-  @md5='6d8c256468a198458b9f08ba6aa1a384'
+  @url='http://erlang.org/download/otp_src_R13B01.tar.gz'
+  @md5='b3db581de6c13e1ec93d74e54a7b4231'
+  
+  def version
+    "R13B01"
+  end
 
   def install
     ENV.deparallelize
@@ -17,4 +31,5 @@ class Erlang <Formula
     system "make"
     system "make install"
   end
+  
 end
