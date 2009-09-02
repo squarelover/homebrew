b8508fa1c0a9c1a7e5ffd0f441147bcb601adcd5
Sean Wolfe <sean@iheartsquares.com>
2009-09-02 10:11:46 -0500

    Some improvements to the Erlang Formula

--- Library/Formula/erlang.rb
+++ Library/Formula/erlang.rb
@@ -13,20 +13,22 @@ class ErlangHtmlDocs <Formula
 end
 
 class Erlang <Formula
-  @version='5.7.2'
+  @version='R13B01'
   @homepage='http://www.erlang.org'
   @url='http://erlang.org/download/otp_src_R13B01.tar.gz'
   @md5='b3db581de6c13e1ec93d74e54a7b4231'
 
   def install
     ENV.deparallelize
-    system "./configure", "--disable-debug",
-                          "--prefix='#{prefix}'",
+    config_flags = ["--disable-debug",
+                          "--prefix=#{prefix}",
                           "--enable-kernel-poll",
                           "--enable-threads",
                           "--enable-dynamic-ssl-lib",
                           "--enable-smp-support",
-                          "--enable-hipe"
+                          "--enable-hipe"]
+    config_flags << "--enable-darwin-64bit" if hw_model == :core2 and os_version == :snow_leopard
+    system "./configure", *config_flags
     system "make"
     system "make install"
     
