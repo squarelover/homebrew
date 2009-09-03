4d2f7b2df19f2625a52def30c4f2c45a1e01fd9d
Sean Wolfe <sean@iheartsquares.com>
2009-09-03 14:10:05 -0500

    Some more tweaks to the erlang build, crypto still broke.

--- Library/Formula/erlang.rb
+++ Library/Formula/erlang.rb
@@ -1,13 +1,15 @@
 require 'brewkit'
 
 class ErlangManuals <Formula
-  @version='5.7.2'
+  @version='R13B01'
   @homepage='http://www.erlang.org'
   @url='http://erlang.org/download/otp_doc_man_R13B01.tar.gz'
   @md5='fa8f96159bd9a88aa2fb9e4d79d7affe'
 end
 
 class ErlangHtmlDocs <Formula
+  @version='R13B01'
+  @homepage='http://www.erlang.org'
   @url='http://erlang.org/download/otp_doc_html_R13B01.tar.gz'
   @md5='42cb55bbfa5dc071fd56034615072f7a'
 end
@@ -18,13 +20,22 @@ class Erlang <Formula
   @url='http://erlang.org/download/otp_src_R13B01.tar.gz'
   @md5='b3db581de6c13e1ec93d74e54a7b4231'
 
+  def deps
+    LibraryDep.new 'icu4c'
+    LibraryDep.new 'openssl'
+  end
+    
   def install
     ENV.deparallelize
+    # erlang needs to build clean on Snow Leopard
+    ENV['CFLAGS']=ENV['CXXFLAGS']=ENV['LDFLAGS']=""
+    openssl_prefix = Formula.factory('openssl').prefix
     config_flags = ["--disable-debug",
                           "--prefix=#{prefix}",
                           "--enable-kernel-poll",
                           "--enable-threads",
                           "--enable-dynamic-ssl-lib",
+                          "--with-ssl=#{openssl_prefix}",
                           "--enable-smp-support",
                           "--enable-hipe"]
     config_flags << "--enable-darwin-64bit" if hw_model == :core2 and os_version == :snow_leopard
@@ -32,7 +43,8 @@ class Erlang <Formula
     system "make"
     system "make install"
     
-    ErlangManuals.new.brew { man.install Dir['man/*'] }
+    if ARGV.include? '--with-mandocs'
+      ErlangManuals.new.brew { man.install Dir['man/*'] }
+    end
   end
-  
 end
